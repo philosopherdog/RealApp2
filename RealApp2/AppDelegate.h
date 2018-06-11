5f9dc06aeec130d4b8b48ad5e8b8f2156c6cbe55
steve thompson <steven.thompson.ca@gmail.com>
2018-06-11 10:36:26 -0400

    Initial Commit

---
+++ RealApp2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealApp2
+//
+//  Created by steve on 2018-06-11.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealApp2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealApp2
+//
+//  Created by steve on 2018-06-11.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
